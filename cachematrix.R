146f0b8569d83bea1227400f229f2b7608c67af4
Lien-H <Lien.Horemans@outlook.com>
2025-08-05 02:46:27 +0200

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## makeCacheMatrix: creates a special matrix object that can store its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL  # initialize inverse cache
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL  # reset cached inverse when matrix changes
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse  # cache the inverse
+  getinverse <- function() inv  # retrieve the cached inverse
+  
+  # Return list of functions
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+## cacheSolve: retrieves inverse matrix if it is already stored, 
+##otherwise it computates it
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()  # get cached inverse
+  if (!is.null(inv)) {
+    message("getting cached inverse")
+    return(inv)  # return cached inverse
+  }
+  
+  mat <- x$get()         # get the matrix
+  inv <- solve(mat, ...) # compute the inverse
+  x$setinverse(inv)      # cache the inverse
+  inv                    # return the inverse
 }
+
+
+## test 1
+my_matrix <- matrix(c(4, 7, 2, 6), nrow = 2)
+cachedMatrix <- makeCacheMatrix(my_matrix)
+inv1 <- cacheSolve(cachedMatrix)
+print(inv1)
+inv2 <- cacheSolve(cachedMatrix)
+print(inv2)
+
+## test 2
+cachedMatrix$set(matrix(c(1, 2, 3, 4), nrow = 2))
+inv3 <- cacheSolve(cachedMatrix)
+print(inv3)
